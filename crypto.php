3f2cdbf94335635a1f753e8b74e21ba5e568a4a4
Steven Soriano <33301703+svensori@users.noreply.github.com>
2018-07-16 16:54:07 +0800

    Create crypto.php

---
+++ crypto.php
@@ -0,0 +1,13 @@
+class Crypto{
+		private static $method = 'AES-256-CBC'; 
+		private static $skey = '?81!-`g}3||vsHm';//
+		private static $siv = 'OC9S?+M%y:pQ2sV';//any
+
+		public static function encrypt($data){
+	        return base64_encode(openssl_encrypt($data, self::$method, hash('sha256', self::$skey), 0, substr(hash('sha256', self::$siv), 0, 16)));
+		}
+
+		public static function decrypt($data){
+			return openssl_decrypt(base64_decode($data), self::$method, hash('sha256', self::$skey), 0, substr(hash('sha256', self::$siv), 0, 16));
+		}
+	}
